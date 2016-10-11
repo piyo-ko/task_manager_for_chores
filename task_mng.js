a1498bcc8cc8a291e496ba678db874ef6ce16ed2
Piyo <piyo-ko@zj8.so-net.ne.jp>
2016-10-11 12:46:01 +0900

    :closed_lock_with_key: :ribbon: :tada: :memo: 入力エラーチェックを追加。コマンド出力欄を拡大。既存タスクの周期・締切を変更する機能を追加。READMEにそれを反映。

--- task_mng.js
+++ task_mng.js
@@ -2,7 +2,7 @@ var cmd_str="";
 
 function record_finished_task() {
   var d=document.in.date_when_task_finished.value;
-  //console.log(d);
+
   var task_types=document.in.finished_task_type;
   var which_type;
   for (var i=0; i<task_types.length; i++) {
@@ -11,12 +11,10 @@ function record_finished_task() {
       break;
     }
   }
-  //console.log(which_type);
+
   var op_list=document.getElementById(which_type + '_tasks');
   var task=op_list.options[op_list.selectedIndex].value;
-  //console.log(task);
-  //var s=d + "," + which_type + "," + task +"\n";
-  //document.in.commands.value += s;
+
   var sql="";
   if (which_type=='regular_weekly' || which_type=='regular_monthly' || which_type=='regular_yearly') {
     sql="insert into regular_chores_rec (reg_chore_id, conducted_on) values (" + task + ", '" + d + "'); ";
@@ -24,32 +22,96 @@ function record_finished_task() {
   } else if (which_type=='irregular_weekly' || which_type=='irregular_monthly' || which_type=='irregular_yearly') {
     sql="update irregular_chores_idx set conducted_on='" + d + "' where irr_chore_id = " + task + ";";
   }
-  //console.log(sql);
+
   cmd_str += "sqlite3 chores.db \"" + sql + "\"\n";
   document.in.commands.value=cmd_str;
 }
 
 function register_new_regular_task() {
-  var sql="insert into regular_chores_idx (validity, period, what_to_do) values (1, " + parseInt(document.in.period_for_regular_task.value).toString() + ", '" + document.in.name_of_new_regular_task.value + "');";
-  cmd_str += "sqlite3 chores.db \"" + sql + "\"\n";
-  document.in.commands.value=cmd_str;
+  var p=parseInt(document.in.period_for_regular_task.value);
+  var n=document.in.name_of_new_regular_task.value;
+  if (isNaN(p) || p < 1) {
+    alert('周期をちゃんと指定してね');
+  } else if (n == '') {
+    alert('定期タスク名を入力してね');
+  } else {
+    var sql="insert into regular_chores_idx (validity, period, what_to_do) values (1, " + p.toString() + ", '" + n + "');";
+    cmd_str += "sqlite3 chores.db \"" + sql + "\"\n";
+    document.in.commands.value=cmd_str;
+  }
 }
 
 function register_new_irregular_task() {
-  var sql="insert into irregular_chores_idx (validity, to_be_done_by, what_to_do) values (1, '" + document.in.deadline_for_irregular_task.value + "', '" + document.in.name_of_new_irregular_task.value + "');";
-  cmd_str += "sqlite3 chores.db \"" + sql + "\"\n";
-  document.in.commands.value=cmd_str;
+  var d=document.in.deadline_for_irregular_task.value;
+  var n=document.in.name_of_new_irregular_task.value;
+  if (isNaN(Date.parse(d))) {
+    alert('日付の形式が変ですよ');
+  } else if (n == '') {
+    alert('非定期タスク名を入力してね');
+  } else {
+    var sql="insert into irregular_chores_idx (validity, to_be_done_by, what_to_do) values (1, '" + d + "', '" + n + "');";
+    cmd_str += "sqlite3 chores.db \"" + sql + "\"\n";
+    document.in.commands.value=cmd_str;
+  }
+}
+
+function change_period() {
+  var task_types=document.in.finished_task_type;
+  var which_type;
+  for (var i=0; i<task_types.length; i++) {
+    if (task_types[i].checked) {
+      which_type=task_types[i].value;
+      break;
+    }
+  }
+  if (which_type=='regular_weekly' || which_type=='regular_monthly' || which_type=='regular_yearly') {
+    var p=parseInt(document.in.new_period_for_regular_task.value);
+    if (isNaN(p) || p < 1) {
+      alert('新しい周期をちゃんと指定してね');
+    } else {
+      var op_list=document.getElementById(which_type + '_tasks');
+      var task=op_list.options[op_list.selectedIndex].value;
+      var sql="update regular_chores_idx set period=" + p.toString() + " where reg_chore_id=" + task + ";";
+      cmd_str += "sqlite3 chores.db \"" + sql + "\"\n";
+      document.in.commands.value=cmd_str;
+    }
+  } else {
+    alert('定期タスクを指定してね');
+  }
+}
+
+function change_deadline() {
+  var task_types=document.in.finished_task_type;
+  var which_type;
+  for (var i=0; i<task_types.length; i++) {
+    if (task_types[i].checked) {
+      which_type=task_types[i].value;
+      break;
+    }
+  }
+  if (which_type=='irregular_weekly' || which_type=='irregular_monthly' || which_type=='irregular_yearly') {
+    var d=document.in.new_deadline_for_irregular_task.value;
+    if (isNaN(Date.parse(d))) {
+      alert('新しい締切をちゃんと指定してね');
+    } else {
+      var op_list=document.getElementById(which_type + '_tasks');
+      var task=op_list.options[op_list.selectedIndex].value;
+      var sql="update irregular_chores_idx set to_be_done_by='" + d + "' where irr_chore_id=" + task + ";";
+      cmd_str += "sqlite3 chores.db \"" + sql + "\"\n";
+      document.in.commands.value=cmd_str;
+    }
+  } else {
+    alert('非定期タスクを指定してね');
+  }
 }
 
 var sel_ids=['regular_weekly_tasks', 'regular_monthly_tasks', 'regular_yearly_tasks', 'irregular_weekly_tasks', 'irregular_monthly_tasks', 'irregular_yearly_tasks'];
 
 function reset_op_color(focused_sel_idx) {
-  //console.log("reset_op_color: " + focused_sel_idx);
   for (var i=0; i<6; i++) {
     if (i!=focused_sel_idx) {
       document.getElementById(sel_ids[i]).selectedIndex=-1;
     }
-    //console.log("i=" + i);
   }
 }
 
@@ -59,7 +121,6 @@ function add_last_command() {
 }
 
 window.onload = function () {
-//  //cmd_str="sqlite3 chores.db <<'CHORES'\n";
   cmd_str="";
   document.in.reset();
 };
