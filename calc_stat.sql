f594475bbad9bcf6a81692a671b99fe31ba041f5
Piyo <piyo-ko@zj8.so-net.ne.jp>
2016-10-16 01:00:03 +0900

    :rocket: 簡易統計用のsqlファイルを試しに追加

---
+++ calc_stat.sql
@@ -0,0 +1,77 @@
+.header ON
+.mode column
+
+.print '**********    2回以上行った作業についての簡易統計    **********'
+.print '   [最初の実行日, 最終実行日, 実行回数, 平均間隔, ID, タスク名]'
+.width 11 11 3 8 2 30
+
+select 
+	min(r.conducted_on) as first, 
+	max(r.conducted_on) as last, 
+	count(r.reg_chore_id) as num, 
+	printf("%2.3f", (julianday(date(max(r.conducted_on))) - julianday(date(min(r.conducted_on)))) / (count(r.reg_chore_id) -1)) as interval,
+	r.reg_chore_id as ID,  
+	i.what_to_do as task
+from regular_chores_rec as r, regular_chores_idx as i
+where r.reg_chore_id=i.reg_chore_id
+group by r.reg_chore_id having count(r.reg_chore_id) > 1
+order by (julianday(date(max(r.conducted_on))) - julianday(date(min(r.conducted_on)))) / (count(r.reg_chore_id) -1) desc;
+
+/*
+select 
+	min(r.conducted_on) as first, 
+	max(r.conducted_on) as last, 
+	count(r.reg_chore_id) as num, 
+	printf("%2.3f", (julianday(date(max(r.conducted_on))) - julianday(date(min(r.conducted_on))) + 1) / count(r.reg_chore_id)) as interval,
+	r.reg_chore_id as ID,  
+	i.what_to_do as task
+from regular_chores_rec as r, regular_chores_idx as i
+where r.reg_chore_id=i.reg_chore_id
+group by r.reg_chore_id having count(r.reg_chore_id) > 1
+order by interval desc;
+
+
+select 
+	min(r.conducted_on) as first, 
+	count(r.reg_chore_id) as num, 
+	(julianday('now') - julianday(date(min(r.conducted_on)))) / count(r.reg_chore_id) as interval,
+	r.reg_chore_id as ID,  
+	i.what_to_do as task
+from regular_chores_rec as r, regular_chores_idx as i
+where r.reg_chore_id=i.reg_chore_id
+group by r.reg_chore_id having count(r.reg_chore_id) > 0
+order by interval;
+
+
+select 
+	min(r.conducted_on) as first, 
+	max(r.conducted_on) as last, 
+	count(r.reg_chore_id) as num, 
+	(julianday(date(max(r.conducted_on))) - julianday(date(min(r.conducted_on)))) as interval,
+	r.reg_chore_id as ID,  
+	i.what_to_do as task
+from regular_chores_rec as r, regular_chores_idx as i
+where r.reg_chore_id=i.reg_chore_id
+group by r.reg_chore_id having count(r.reg_chore_id) > 1 
+order by interval;
+
+select min(conducted_on) as first, max(conducted_on) as last, count(*), reg_chore_id from regular_chores_rec group by reg_chore_id having count(*) > 1 order by reg_chore_id;
+
+select min(conducted_on) as first, max(conducted_on) as last, count(*), reg_chore_id from regular_chores_rec group by reg_chore_id order by reg_chore_id;
+*/
+
+.print ' '
+.print '********  1回だけ行った作業の一覧  ********'
+.print '  [実行日, 定期/非定期の種別, ID, タスク名]'
+.width 11 4 3 40
+select min(r.conducted_on) as date, 'Reg' as typ, r.reg_chore_id as ID, i.what_to_do as task
+from regular_chores_rec as r, regular_chores_idx as i
+where r.reg_chore_id=i.reg_chore_id
+group by r.reg_chore_id having count(r.reg_chore_id) = 1
+union
+select conducted_on as date, 'Irr' as typ, irr_chore_id as ID, what_to_do as task
+from irregular_chores_idx
+where conducted_on is not null and conducted_on is not ''
+order by date desc;
+
+
